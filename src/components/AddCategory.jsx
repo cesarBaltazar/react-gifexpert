96d9a00b29ef8bdc0679dc52cf61e03c304e64a8
Cesar Baltazar <cbaltazar@exact.com.pe>
2023-05-13 16:43:34 -0500

    Primer commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+
+export const AddCategory = ( { onNewCategory } ) => {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const onInputChange = (event) => {
+        setInputValue(event.target.value);
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        
+        if( inputValue.trim().length <=1) return;
+        // setCategories((categories) => [...categories, inputValue] );
+        onNewCategory( inputValue.trim() );
+        setInputValue("");
+    }
+
+  return (
+    <form onSubmit={ onSubmit }>
+        <input 
+            type="text"
+            placeholder="Buscar gifts"
+            value={ inputValue }
+            onChange={ (event) => onInputChange(event) }
+        />
+    </form>
+    
+  )
+}
+
+// AddCategory.protoTypes = {
+    
+// }
